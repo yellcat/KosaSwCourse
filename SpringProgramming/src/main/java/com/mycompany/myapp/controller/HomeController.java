810fe5232a9bc21d5a67e8fa97f4599d496b86ec
egg_kim <yellcat@naver.com>
2015-09-02 11:17:48 +0900

    Board Controller 만들었당

--- SpringProgramming/src/main/java/com/mycompany/myapp/controller/HomeController.java
+++ SpringProgramming/src/main/java/com/mycompany/myapp/controller/HomeController.java
@@ -14,12 +14,11 @@ public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		System.out.println("HomeController-Home()");//프로그램을 만든다면 지워주어야 함
+	@RequestMapping("/")
+	public String home() {
+		/*System.out.println("HomeController-Home()");//프로그램을 만든다면 지워주어야 함*/	
 		logger.info("HomeController-Home()");
-		logger.debug("HomeController-Home()");//log를 기록하지 않는다고 한다면 지워주지 않아도 된다
-		
+		/*logger.debug("HomeController-Home()");//log를 기록하지 않는다고 한다면 지워주지 않아도 된다*/	
 		return "home";
 	}
 	
